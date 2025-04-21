a5530f4942dae655cd5bd80d486b47d359276457
Sebastiao <sebastiao.filho2301@gmail.com>
2025-04-21 00:13:22 -0300

    Initial commit: Projeto Spring Boot da API Sustentável

---
+++ api-sustentavel/src/main/java/com/sebastiao/api_sustentavel/ApiSustentavelApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiao.api_sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSustentavelApplication.class, args);
+	}
+
+}
---
+++ api-sustentavel/src/test/java/com/sebastiao/api_sustentavel/ApiSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiao.api_sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
